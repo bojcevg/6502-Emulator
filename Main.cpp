3479a4a9dd81884367e692a591ffba839bad07e5
bojcevg <bojcevg@mcmaster.ca>
2025-01-07 15:43:13 -0500

    instructions are working!(mini little inline program)

--- Main.cpp
+++ Main.cpp
@@ -9,7 +9,12 @@ struct Memory
     static const int MAX_MEM = 1024 * 64; //This is because there are 1024 8 byte addresses in the emulated memory
     Byte Data[MAX_MEM];
 
-    Byte operator[](int index)
+    Byte operator[](int index) const //READ
+    {
+        return Data[index];
+    }
+
+    Byte& operator[](int index) //WRITE
     {
         return Data[index];
     }
@@ -55,18 +60,38 @@ struct CPU
         mem.intitialize();
     }
 
-    Byte FetchByte( Memory& mem )
+    Byte FetchByte( Memory& mem, int& cycles )
     {
         Byte data = mem[PC]; //pulls the current instruction waiting to be executed
         PC++; //moves the program counter to the next instruction
+        cycles--;
         return data;
-
     }
+
+    //opcodes for Load Accumulator
+    static const Byte INS_LDA_IM = 0xA9;
+
     void execute(Memory& mem, int cycles)
     {
-        for(int i  = cycles; i > 0; i--)
+        while (cycles > 0)
         {
-            Byte instruction = FetchByte( mem );
+            Byte instruction = FetchByte( mem , cycles );
+            switch( instruction )
+            {
+                case INS_LDA_IM:
+                {
+                    Byte value = FetchByte( mem , cycles ); //Immediate means that the next spot in memory is the info we want, and since FB increments the pointer by 1, we can just call FB again.
+                    A = value; //Loads into the accumulator register
+                    Z = (A==0);
+                    N = (A & 0b10000000) > 0; // sets the negative flag if the MSD is 1
+                } break;
+
+                default:
+                {
+                    printf("Instruction not handled: %d", instruction);
+                } break;
+            }
+
         }
     }
 
@@ -79,6 +104,12 @@ int main()
     Memory mem;
 
     cpu.reset(mem);
+
+    // inline program 
+    mem[0xFFFC] = CPU::INS_LDA_IM;
+    mem[0xFFFD] = 0x42;
+    //end
+    
     cpu.execute(mem, 2);
     return 0;
 }
